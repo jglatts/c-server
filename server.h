0529126832bc68b8099bd9d81415dd9332b03439
John Glatts <john@z-axiscc.com>
2021-12-04 09:16:19 -0500

    add the WaitList struct

--- server.h
+++ server.h
@@ -1,13 +1,19 @@
 #ifndef SERVER__H
 #define SERVER__H
 
-#include <pthread.h>
-
-#define DICT_SIZE           100000 
-#define DEFAULT_PORT        8888	
+#define DICT_SIZE           100000
+#define DEFAULT_PORT        8888
 #define DEFAULT_DICTIONARY  "/usr/share/dict/words"
-#define MAX_WORK_THREADS    10
-#define QUEUE_SIZE          8 
+#define MAX_WORK_THREADS    8
+#define QUEUE_SIZE          8
+
+#define for_each_in_waitlist(list) \
+        for(WaitList* n = list->head; n; n = n->next;)
+
+typedef struct WaitList {
+        int fd;
+        struct WaitList* next;
+} WaitList;
 
 void  init_dict(int argc, char** argv);
 void  init_network(int*);
@@ -15,6 +21,7 @@ void  init_worker_threads();
 void  init_logger_thread();
 void  init_threads_synch();
 void  add_new_socket(int);
+void  add_wait_list_node(int);
 void  work_queue_add(int);
 void  log_queue_add(char*);
 void  check_spelling(int, char*);
